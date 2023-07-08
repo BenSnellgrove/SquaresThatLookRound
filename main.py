33c6055eb31f4f48c3f91758b72c974400356dc3
Ben Snellgrove <ben.e.snellgrove@gmail.com>
2023-07-08 20:49:58 +0100

    Added Triangles The mouse now exists as a concept

--- main.py
+++ main.py
@@ -3,8 +3,25 @@ import sys
 
 pygame.init()
 
+# TODO loose plan
+#  cube centred around 0, 0, 0 for simplicity
+#  click and drag rotates - simple vector transformation
+#  'camera' has static position at somewhere like 10, 0, 0
+#  xy is flat, z is up
+#  can draw based on distance from camera, or have one sided 'textures'
+#  probably the former first, seems simpler
+
+
 # Colours
 colour_background = (255, 255, 255)
+
+# My colour palette is rubik's cube
+colour_grey = (191, 191, 191)
+colour_yellow = (255, 255, 63)
+colour_green = (63, 255, 63)
+colour_blue = (63, 63, 255)
+colour_red = (255, 63, 63)
+colour_orange = (255, 191, 63)
 
 # Game Setup
 FPS = 60  # because higher refresh rates aren't real
@@ -17,7 +34,7 @@ pygame.display.set_caption("Cubes that look round? Idk man")
 
 
 class Coord:
-    def __init__(self, x: int, y: int, z: int):
+    def __init__(self, x: float, y: float, z: float):
         self.x = x
         self.y = y
         self.z = z
@@ -29,14 +46,31 @@ class Line:
         self.end = end
 
         self.length = (
-            (end.x - start.x) ** 2 +
-            (end.y - start.y) ** 2 +
-            (end.z - start.z) ** 2
-        ) ** 0.5
+                              (end.x - start.x) ** 2 +
+                              (end.y - start.y) ** 2 +
+                              (end.z - start.z) ** 2
+                      ) ** 0.5
+
+
+# If you think about it, everything is a triangle
+class Triangle:
+    def __init__(self, a: Coord, b: Coord, c: Coord):
+        self.vertices = [a, b, c]
+
+        # 3 points sit on a plane so this is the centre of that 'plane'
+        # You will also learn that I love list comprehensions
+        self.centre = Coord(
+            sum([c.x for c in self.vertices]) / 3,
+            sum([c.y for c in self.vertices]) / 3,
+            sum([c.z for c in self.vertices]) / 3
+        )
 
 
 def main():
     looping = True
+
+    mouseDown = None
+    mouseUp = None
 
     while looping:
 
@@ -45,6 +79,16 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouseDown = pygame.mouse.get_pos()
+                print(mouseDown)
+
+            if event.type == pygame.MOUSEBUTTONUP:
+                mouseUp = pygame.mouse.get_pos()
+                print(mouseUp)
+
+                # do bounds checking
 
         # Processing
         # This section will be built out later
