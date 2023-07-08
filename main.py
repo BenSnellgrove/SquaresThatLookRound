d8fcc834106427f403d31a7bc65a8cb8766c9e46
Ben Snellgrove <ben.e.snellgrove@gmail.com>
2023-07-08 20:26:34 +0100

    Setup things

---
+++ main.py
@@ -0,0 +1,69 @@
+import pygame
+import sys
+
+pygame.init()
+
+# Colours
+colour_background = (255, 255, 255)
+
+# Game Setup
+FPS = 60
+fpsClock = pygame.time.Clock()
+WINDOW_WIDTH = 400
+WINDOW_HEIGHT = 300
+
+gameWindow = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Cubes that look round? Idk man")
+
+
+class Coord:
+    def __init__(self, x: int, y: int, z: int):
+        self.x = x
+        self.y = y
+        self.z = z
+
+
+class Line:
+    def __init__(self, start: Coord, end: Coord):
+        self.start = start
+        self.end = end
+
+        self.length = (
+            (end.x - start.x) ** 2 +
+            (end.y - start.y) ** 2 +
+            (end.z - start.z) ** 2
+        ) ** 0.5
+
+cube_coords = [
+    Coord(0, 0, 0),
+]
+
+
+# The main function that controls the game
+def main():
+    looping = True
+
+    # The main game loop
+    while looping:
+
+        # Input handling
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        # Processing
+        # This section will be built out later
+
+        # Render elements of the game
+        # bottom up
+
+        gameWindow.fill(colour_background)
+
+        # force window update
+        pygame.display.update()
+        fpsClock.tick(FPS)
+
+
+if __name__ == '__main__':
+    main()
